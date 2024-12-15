8debffa4889b34e2a1ba3aa69c88f300b6f60d75
intscoula <167413860+intscoula@users.noreply.github.com>
2024-12-15 14:45:47 +0700

    Update lock.h

--- lock.h
+++ lock.h
@@ -1,1 +1,8 @@
+#pragma once
+#include <string>
 
+bool isLocked(const DatabaseManager& dbManager, const std::string& tableName);
+
+void locking(const DatabaseManager& dbManager, const std::string& tableName);
+
+void unlocking(const DatabaseManager& dbManager, const std::string& tableName);
